1756bd79861f62e95e179442403f1fd068404926
Tchr1s <csepulveda232001@gmail.com>
2025-03-19 18:43:18 -0300

    correofuncional

--- js/script.js
+++ js/script.js
@@ -2,3 +2,22 @@ const tooltips = document.querySelectorAll('.tt')
 tooltips.forEach(t => {
     new bootsrap.Tooltip(t)
 })
+
+function handleSubmit(event) {
+    event.preventDefault(); // Evita el comportamiento predeterminado de envío del formulario
+
+    // Muestra la ventana modal
+    var confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
+    confirmationModal.show();
+
+    // Opcional: Aquí puedes agregar código para enviar el formulario a Netlify usando fetch
+    // Ejemplo:
+    fetch(event.target.action, {
+        method: 'POST',
+        body: new FormData(event.target),
+    }).then(response => {
+        console.log('Formulario enviado', response);
+    }).catch(error => {
+        console.error('Error al enviar el formulario', error);
+    });
+}
