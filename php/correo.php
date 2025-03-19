6c334aee44c2eeb7c3438988add41f9616bdf5f8
Tchr1s <csepulveda232001@gmail.com>
2025-03-19 17:27:50 -0300

    correoprueba2

--- php/correo.php
+++ php/correo.php
@@ -6,6 +6,6 @@ $asunto = $_POST['asunto'];
 $comentario = $_POST['comentario'];
 
 var_dump($nombre);
-$rta = mail('csepulveda232001@gmail.com', $asunto, $comentario)
-var_dump($rta)
+$rta = mail('csepulveda232001@gmail.com', $asunto, $comentario);
+var_dump($rta);
 ?>
